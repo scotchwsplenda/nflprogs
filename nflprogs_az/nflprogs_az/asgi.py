773d563f9631f60ff8c678052fc1433883f03631
scotchwsplenda <lockwood.oliver@gmail.com>
2021-09-02 13:54:24 -0700

    1st

---
+++ nflprogs_az/nflprogs_az/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nflprogs_az project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nflprogs_az.settings')
+
+application = get_asgi_application()
